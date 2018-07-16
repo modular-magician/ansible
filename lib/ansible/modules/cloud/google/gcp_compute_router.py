8a60d59e06ac0bbbafe05fd60625406b391947d3
Dana Hoffman <danahoffman@google.com>
2018-07-16 20:44:29 +0000

    Magic Modules changes.

--- lib/ansible/modules/cloud/google/gcp_compute_router.py
+++ lib/ansible/modules/cloud/google/gcp_compute_router.py
@@ -301,7 +301,7 @@ def create(module, link, kind):
 
 def update(module, link, kind):
     auth = GcpSession(module, 'compute')
-    return wait_for_operation(module, auth.put(link, resource_to_request(module)))
+    return wait_for_operation(module, auth.patch(link, resource_to_request(module)))
 
 
 def delete(module, link, kind):
