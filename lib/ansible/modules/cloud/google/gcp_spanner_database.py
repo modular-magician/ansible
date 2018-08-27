768c75d9cbdd30f49eab8080001e902a8e7c522e
Alex Stephen <alexstephen@google.com>
2018-08-27 18:40:06 +0000

    Magic Modules changes.

--- lib/ansible/modules/cloud/google/gcp_spanner_database.py
+++ lib/ansible/modules/cloud/google/gcp_spanner_database.py
@@ -48,7 +48,7 @@ options:
         default: 'present'
     name:
         description:
-            - A unique identifier for the instance, which cannot be changed after the instance
+            - A unique identifier for the database, which cannot be changed after the instance
               is created. Values are of the form projects/<project>/instances/[a-z][-a-z0-9]*[a-z0-9].
               The final segment of the name must be between 6 and 30 characters in length.
         required: false
@@ -93,7 +93,7 @@ EXAMPLES = '''
 RETURN = '''
     name:
         description:
-            - A unique identifier for the instance, which cannot be changed after the instance
+            - A unique identifier for the database, which cannot be changed after the instance
               is created. Values are of the form projects/<project>/instances/[a-z][-a-z0-9]*[a-z0-9].
               The final segment of the name must be between 6 and 30 characters in length.
         returned: success
